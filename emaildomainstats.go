807d5509abe0e52db44e3b5b382452883690eafa
Imdad Hoque <imdad.hoque@mirrorweb.com>
2021-07-26 11:40:33 +0100

    Clarify wording

--- emaildomainstats.go
+++ emaildomainstats.go
@@ -1,6 +1,6 @@
 /*
-This package is required to provide functionality to read the supplied customer_data.csv file
-and return a sorted data structure of your choice containing email domains along with the number of
+This package is required to provide functionality to process the supplied customer_data.csv file
+and return a sorted (by email domain) data structure of your choice containing the email domains along with the number of
 customers for each domain. Any errors should be logged (or handled) or returned to the consumer of this package.
 Performance matters, the sample file may only contains 1K lines but it could be 1m lines or run on a small machine.
  */
