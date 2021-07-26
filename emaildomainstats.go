f07e3a02d252b8709a564df6a7d73af0b4d477b3
Imdad Hoque <imdad.hoque@mirrorweb.com>
2021-07-26 11:10:07 +0100

    Mirrorweb Golang tech test

---
+++ emaildomainstats.go
@@ -0,0 +1,8 @@
+/*
+This package is required to provide functionality to read the supplied customer_data.csv file
+and return a sorted data structure of your choice containing email domains along with the number of
+customers for each domain. Any errors should be logged (or handled) or returned to the consumer of this package.
+Performance matters, the sample file may only contains 1K lines but it could be 1m lines or run on a small machine.
+ */
+
+package emaildomainstats
