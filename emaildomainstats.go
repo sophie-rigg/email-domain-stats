acfd2ae7b80aed5ad711d3edbad0530b5105022e
Imdad Hoque <imdadhoque@imdads-mbp.home>
2022-04-26 12:21:42 +0100

    feat: improve description

--- emaildomainstats.go
+++ emaildomainstats.go
@@ -1,8 +1,12 @@
 /*
-This package is required to provide functionality to process the supplied customer_data.csv file
-and return a sorted (by email domain) data structure of your choice containing the email domains along with the number of
-customers for each domain. Any errors should be logged (or handled) or returned to the consumer of this package.
-Performance matters, the sample file may only contains 1K lines but it could be 1m lines or run on a small machine.
+This package is required to provide functionality to process a csv file and return a sorted (by email domain) data
+structure of your choice containing the email domains along with the number of customers for each domain. The customer_data.csv
+file provides an example csv file to work with. Any errors should be logged (or handled) or returned to the consumer of
+this package. Performance matters, the sample file may only contain 1K lines but the package may be expected to be used on
+files with 10 million lines or run on a small machine.
+
+Write this package as you normally would for any production grade code that would be deployed to a live system.
+
 Please stick to using the standard library.
  */
 
