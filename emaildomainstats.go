4e75eb1b1c6963486df4c85861033d06bba6536d
Imdad Hoque <imdadhoque@imdads-mbp.home>
2022-04-26 11:32:16 +0100

    Update requirements

--- emaildomainstats.go
+++ emaildomainstats.go
@@ -3,6 +3,7 @@ This package is required to provide functionality to process the supplied custom
 and return a sorted (by email domain) data structure of your choice containing the email domains along with the number of
 customers for each domain. Any errors should be logged (or handled) or returned to the consumer of this package.
 Performance matters, the sample file may only contains 1K lines but it could be 1m lines or run on a small machine.
+Please stick to using the standard library.
  */
 
 package emaildomainstats
